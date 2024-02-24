e4f5eb4f0116f12b052990760afcc226fe74cf4b
Malitha supun <86181484+MalithaSupun@users.noreply.github.com>
2024-02-24 20:31:17 +0530

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import {BsFillMoonStarsFill} from 'react-icons/bs';
 import { AiFillTwitterCircle, AiFillLinkedin, AiFillYoutube } from "react-icons/ai";
+import Image from "next/image";
+import deved from "../public/dev-ed-wave.png";
 
 
 
@@ -25,17 +27,20 @@ export default function Home() {
                     </li>
                   </ul>
                   </nav>
-                  <div>
-                    <h2>Malitha Supun</h2>
-                    <h3>Full-stack developer</h3>
-                    <p>
+                  <div className="text-center p-10">
+                    <h2 className="text-5xl py-2 text-teal-600 font-medium">Malitha Supun</h2>
+                    <h3 className="text-2xl py-2">Full-stack developer</h3>
+                    <p className="text-md py-5 leading-8 text-gray-800">
                         Freelancer providing services for programming and design content needs.Join me down below and let's get cracking!
                     </p>
                   </div>
-                  <div>
+                  <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
                     <AiFillTwitterCircle />
                     <AiFillLinkedin />
                     <AiFillYoutube />
+                </div>
+                <div className="" >
+                <Image src={deved} />
                 </div>
                 </section>
             </main>
