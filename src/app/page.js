ed66b6d4bddfe89374d1557702fedb2eee28ec0c
Malitha supun <86181484+MalithaSupun@users.noreply.github.com>
2024-02-24 16:22:55 +0530

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,10 @@
 import Head from "next/head";
+import {BsFillMoonStarsFill} from 'react-icons/bs';
+import {AIFillTwitterCircle, 
+        AiFillLinkedin, 
+        AiFillTwitterCircle, 
+        AiFillYoutube,
+     } from "react-icons/ai";
 
 
 export default function Home() {
@@ -9,13 +15,32 @@ export default function Home() {
                 <meta name="Description" content="Genarated by created"></meta>
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
-            <main>
+            <main className="bg-white px-10">
                 <section className="min-h-screen">
-                  <h1>developedbyed</h1>
-                  <ul>
-                    <li></li>
-                    <li> <a href="#">Resurme</a></li>
+                    <nav className="py-10 mb-12 flex justify-between">
+                  <h1 className="text-xl font-burtons">developedbyed</h1>
+                  <ul className="flex items-center">
+                    <li>
+                        <BsFillMoonStarsFill className="cursor-pointer text-2xl"/>
+                    </li>
+                    <li> 
+                        <a className="bg-gredient-to-r bg-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href="#">Resurme</a>
+                    </li>
                   </ul>
+                  </nav>
+                  <div>
+                    <h2>Malitha Supun</h2>
+                    <h3>Full-stack developer</h3>
+                    <p>
+                        Freelancer providing services for programming and design content needs.Join me down below and let's get cracking!
+                    </p>
+                  </div>
+                  <div>
+                    <AiFillTwitterCircle/>
+                    <AiFillLinkedin/>
+                    <AiFillYoutube/>
+
+                  </div>
                 </section>
             </main>
         </div>
