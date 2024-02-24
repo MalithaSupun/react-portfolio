1d6f33d1c65739585216fb7b23618ac9c6270d61
Malitha supun <86181484+MalithaSupun@users.noreply.github.com>
2024-02-24 20:52:29 +0530

    update

--- src/app/page.js
+++ src/app/page.js
@@ -39,8 +39,8 @@ export default function Home() {
                     <AiFillLinkedin />
                     <AiFillYoutube />
                 </div>
-                <div className="" >
-                <Image src={deved} />
+                <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden" >
+                <Image src={deved} layout="fill" objectFit="cover"/>
                 </div>
                 </section>
             </main>
