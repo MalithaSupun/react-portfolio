63efc7343b829431fde570934eb25156285c1b43
Malitha supun <86181484+MalithaSupun@users.noreply.github.com>
2024-02-24 12:35:09 +0530

    first commit

---
+++ api/index.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+
+export default function Home() {
+    return (
+        <div>
+            <Head>
+                <title>Malitha Supun Portfolio</title>
+                <meta name="Description" content="Genarated by created"></meta>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+            <main>
+                <section className=" bg-blue-700 min-h-screen">
+
+                </section>
+            </main>
+        </div>
+    );
+}
---
+++ src/app/page.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+
+
+export default function Home() {
+    return (
+        <div>
+            <Head>
+                <title>Malitha Supun Portfolio</title>
+                <meta name="Description" content="Genarated by created"></meta>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+            <main>
+                <section className="min-h-screen">
+                  <h1>developedbyed</h1>
+                  <ul>
+                    <li></li>
+                    <li> <a href="#">Resurme</a></li>
+                  </ul>
+                </section>
+            </main>
+        </div>
+    );
+}
